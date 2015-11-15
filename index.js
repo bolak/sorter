40633dd799e0279d7b0de9e457fc95c2242234eb
Sean Bolak <hello@seanbolak.com>
2015-11-15 13:29:42 -0500

    added insertion sort and test.

--- index.js
+++ index.js
@@ -3,19 +3,19 @@
 let sorter = {
   bubble: function(array) {
 
-    let i = 0,
-        n = array.length,
-        swapped;
+    let i,
+        swapped,
+        n = array.length;
 
     do {
       swapped = false;
 
-      for(i; i < n - 1; i++){
+      for(i = 0; i < n - 1; i++){
         var indexOne = i,
             indexTwo = i + 1;
 
         if(array[indexOne] > array[indexTwo]) {
-          array = this.arraySwap(array, indexOne, indexTwo);
+          array = this.swap(array, indexOne, indexTwo);
           swapped = true;
         }
       }
@@ -25,10 +25,20 @@ let sorter = {
     return array;
 
   },
-  insertion: function() {
+  insertion: function(array) {
+   let n = array.length;
 
+   for(var i = 1; i < n; ++i) {
+     var temp = array[i];
+     var j = i - 1;
+     for(; j >= 0 && array[j] > temp; --j) {
+       array[j+1] = array[j];
+     }
+     array[j+1] = temp;
+   }
+   return array;
   },
-  arraySwap: function(array, indexOne, indexTwo) {
+  swap: function(array, indexOne, indexTwo) {
 
     let temp = array[indexOne];
     array[indexOne] = array[indexTwo];
